a38c43f0643fdec5ca3dfced853be0d247de5770
DaniilShapovalov <91855245+DaniilShapovalov@users.noreply.github.com>
2023-09-03 19:33:45 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const menuBtn = document.querySelector('.menu__btn');
+const menuMobile = document.querySelector('.header__menu-list');
+
+menuBtn.addEventListener('click', ()=> {
+    menuMobile.classList.toggle('menu--open');
+})
+
+
+const swiperOne = new Swiper('.feedback__slider', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+    },
+});
+
+const swiperTwo = new Swiper('.certificates__slider', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+    },
+    slidesPerView: 1,
+});
+
+const accordeon = document.querySelector('.accordeon');
+const accordeonTitles = accordeon.querySelectorAll('.accordeon__title');
+
+accordeonTitles.forEach.call(accordeonTitles, function (accordeonTitle){
+    accordeonTitle.addEventListener('click', function(){
+
+        const currentText = accordeonTitle.parentElement.querySelector('.accordeon__text');
+        accordeonTitle.classList.toggle('accordeon__title--active');
+        currentText.classList.toggle('accordeon__text--visible');
+
+        if(currentText.classList.contains('accordeon__text--visible')) {
+            currentText.style.maxHeight = currentText.scrollHeight + 'px'
+        }   else{
+            currentText.style.maxHeight = null
+        }
+    })
+})
